ffc4bbbdd813e0069c6d49c1ebcaf198c487c28b
TeslaX <bartlomiej.malarz@gmail.com>
2019-10-26 22:22:11 +0200

    merged arrivals+departures

--- src/Controller/InfopasazerController.php
+++ src/Controller/InfopasazerController.php
@@ -22,108 +22,128 @@ class InfopasazerController extends AbstractController
     }
 
     /**
-     * @Route("/infopasazer/allArrivals/{station}")
+     * @Route("/infopasazer/trains/{type}/{station}")
      * @param Request $request
      * @return Response
      */
-    public function allArrivals(Request $request)
+    public function getTrains(Request $request)
     {
-
-        $situation = 'arrivals';
-        $situation = ($situation == 'arrivals');
+        $response = new Response();
+        if (!in_array($request->attributes->get('type'), ['arrivals', 'departures'])) {
+            $response->setContent(json_encode(['error' => 'Zły parametr arrivals/departures']));
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
+        }
+        $arrivals = ('arrivals' == $request->attributes->get('type'));
 
         $stationId = $request->attributes->get('station');
 
-        if(!$stationId) $stationId = 33605; //Warszawa Centralna
-        $html = file_get_contents('https://infopasazer.intercity.pl/?p=station&id='.$stationId); //73312
+        if (!$stationId) $stationId = 33605; //Warszawa Centralna
+        $html = @file_get_contents('https://infopasazer.intercity.pl/?p=station&id=' . $stationId); //73312
+        if ($html === FALSE) {
+            $response->setContent(json_encode(['error' => 'Brak połączenia z serwerem infopasażera']));
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
+        }
         $crawler = new Crawler($html);
-        $lastUpdate = str_replace('Aktualizacja: ','',$crawler->filter('div.CustomColor-06 p')->text());
+
+        if ($crawler->filter('div.error')->count() != 0) {
+            $response->setContent(json_encode(['error' => $crawler->filter('div.error')->first()->text()]));
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
+        }
+
+        $lastUpdate = str_replace('Aktualizacja: ', '', $crawler->filter('div.CustomColor-06 p')->text());
         $currentStation = trim($crawler->filter('p.h4')->first()->text());
-        $currentStation = str_replace("Rozkład stacyjny dla stacji ","",$currentStation);
-        //$lastUpdate = date_create_from_format("Y-m-d H:i",$lastUpdate);
-        $arrivalTable = $crawler->filter('table.table.table-delay.mbn tbody')->first()->html();
+        $currentStation = str_replace("Rozkład stacyjny dla stacji ", "", $currentStation);
+        if ($arrivals) {
+            $scheduleTable = $crawler->filter('table.table.table-delay.mbn tbody')->first()->html();
+        } else {
+            $scheduleTable = $crawler->filter('table.table.table-delay.mbn tbody')->last()->html();
+        }
 
-        $crawler = new Crawler($arrivalTable);
-        $trains = $crawler->filter('tr')->each(function($tr,$i) {return $tr->filter('td span')->each(function($td,$i) {return trim($td->html());});});
 
-        $indexNames = ['trainId','trainNo'];
-        $trainAA = ['currentStation' => $currentStation, 'lastUpdate' => $lastUpdate];
-        //$trainAA = [];
-        foreach($trains as $tr) {
+        $crawler = new Crawler($scheduleTable);
+        $trains = $crawler->filter('tr')->each(function ($tr, $i) {
+            return $tr->filter('td span')->each(function ($td, $i) {
+                return trim($td->html());
+            });
+        });
+
+        $trainsHeader = ['currentStation' => $currentStation, 'lastUpdate' => $lastUpdate];
+        $trainAA = [];
+        foreach ($trains as $tr) {
             $thisTrain = [];
-            foreach($tr as $idx=>$td) {
-                if($idx == 0) {
-                    $trainDetails = explode("\"",$td);
-                    $thisTrain['trainId'] = str_replace("?p=train&amp;id=","",$trainDetails[1]);
-                    $trainDetails = str_replace("<br>",";",$td);
+            foreach ($tr as $idx => $td) {
+                if ($idx == 0) {
+                    $trainDetails = explode("\"", $td);
+                    $thisTrain['trainId'] = str_replace("?p=train&amp;id=", "", $trainDetails[1]);
+                    $trainDetails = str_replace("<br>", ";", $td);
                     $trainDetails = trim(strip_tags($trainDetails));
-                    $trainDetails = explode(';',$trainDetails);
+                    $trainDetails = explode(';', $trainDetails);
                     $thisTrain['trainNo'] = $trainDetails[0];
-                    if(count($trainDetails)>1) $thisTrain['trainName'] = $trainDetails[1]; else $thisTrain['trainName'] = "";
+                    if (count($trainDetails) > 1) $thisTrain['trainName'] = $trainDetails[1]; else $thisTrain['trainName'] = "";
                 }
-                if($idx == 1) {
+                if ($idx == 1) {
                     $thisTrain['company'] = trim(strip_tags($td));
                 }
-                if($idx == 2) {
-                    $thisTrain['arrivalTime'] = $td;
+                if ($idx == 2) {
+                    $thisTrain['scheduleTime'] = $td;
                 }
-                if($idx == 3) {
-                    $trainDetails = explode(' - ',$td);
+                if ($idx == 3) {
+                    $trainDetails = explode(' - ', $td);
                     $thisTrain['from'] = $trainDetails[0];
                     $thisTrain['to'] = $trainDetails[1];
                     $thisTrain['via'] = [];
-                        if($thisTrain['to']!=$currentStation) {
-                            $html = file_get_contents('https://infopasazer.intercity.pl/?p=train&id='.$thisTrain['trainId']);
-                            $crawler2 = new Crawler($html);
-                            $delayTable = $crawler2->filter('table.table-delay tbody')->first()->html();
-                            $crawler2 = new Crawler($delayTable);
-                            $stationsTable = $crawler2->filter('tr')->each(function($tr,$i) {return $tr->filter('td span')->each(function($td,$i) {return trim($td->text());});});
-                            foreach($stationsTable as $stations) {
-                                array_push($thisTrain['via'],trim($stations[3]));
-                            }
-                            $whereStation = array_search($currentStation,$thisTrain['via']);
-                            $howManyVia = count($thisTrain['via']);
-                            if($whereStation) {
-                                foreach($thisTrain['via'] as $idx3=>$tvia) {
-                                    if($idx3<=$whereStation) unset($thisTrain['via'][$idx3]);
-                                    if($idx3==$howManyVia-1) unset($thisTrain['via'][$idx3]);
-                                }
-                              $thisTrain['via'] = array_values($thisTrain['via']);  //does not work and don't know why :(
-                            }
+
+                    //via
+                    $html = file_get_contents('https://infopasazer.intercity.pl/?p=train&id=' . $thisTrain['trainId']);
+                    $crawler2 = new Crawler($html);
+                    $delayTable = $crawler2->filter('table.table-delay tbody')->first()->html();
+                    $crawler2 = new Crawler($delayTable);
+                    $stationsTable = $crawler2->filter('tr')->each(function ($tr, $i) {
+                        return $tr->filter('td span')->each(function ($td, $i) {
+                            return trim($td->text());
+                        });
+                    });
+                    foreach ($stationsTable as $stations) {
+                        array_push($thisTrain['via'], trim($stations[3]));
+                    }
+                    $whereStation = array_search($currentStation, $thisTrain['via']);
+                    $howManyVia = count($thisTrain['via']);
+                    foreach ($thisTrain['via'] as $idx3 => $tvia) {
+                        if (($thisTrain['from'] == $tvia) || ($idx3 == $howManyVia - 1)) unset($thisTrain['via'][$idx3]);
+
+                        if ($arrivals) {
+                            if ($idx3 >= $whereStation) unset($thisTrain['via'][$idx3]); //arrivals
+                        } else {
+                            if ($idx3 <= $whereStation) unset($thisTrain['via'][$idx3]); //departures
+
                         }
+                    }
+                    $thisTrain['via'] = array_values($thisTrain['via']);
                 }
-                if($idx == 4) {
-                    $thisTrain['arrivalTime'] .=' '.$td;
+                if ($idx == 4) {
+                    $thisTrain['scheduleTime'] .= ' ' . $td;
                 }
-                if($idx == 5) {
-                    $thisTrain['delayTime'] = str_replace(" min","",$td);
-                    $realDate = date_create_from_format('Y-m-d H:i',$thisTrain['arrivalTime']);
-                    $realDate->modify("+ ".$thisTrain['delayTime']." minutes");
-                    $thisTrain['arrivalRealTime'] = $realDate->format("Y-m-d H:i");
+                if ($idx == 5) {
+                    $thisTrain['delayTime'] = str_replace(" min", "", $td);
+                    $realDate = date_create_from_format('Y-m-d H:i', $thisTrain['scheduleTime']);
+                    $realDate->modify("+ " . $thisTrain['delayTime'] . " minutes");
+                    $thisTrain['realTime'] = $realDate->format("Y-m-d H:i");
                 }
             }
-            array_push($trainAA,$thisTrain);
+            array_push($trainAA, $thisTrain);
         }
-        dd($trainAA);
+
+        $json = $trainsHeader + ['trains' => array_values($trainAA)]; //remove pseudo-array-keys
+        dd($json);
         $response = new Response();
 
-        $response->setContent(json_encode($trainAA));
-        $response->headers->set('Content-Type', 'application/json');
-        return $response;
-    }
-
-    /**
-     * @Route("/infopasazer/allDepartures/{station}")
-     * @param Request $request
-     * @return Response
-     */
-    public function allDepartures(Request $request)
-    {
-        $response = new Response();
-        $json = ['data' => 123];
         $response->setContent(json_encode($json));
         $response->headers->set('Content-Type', 'application/json');
         return $response;
     }
+    
 
 }
