a6c35c9322d7beab47daf41060645fa002a14808
Bartłomiej Malarz <bartlomiej.malarz@gmail.com>
2023-11-05 00:46:58 +0100

    update to symfony 6.3 and php 8.2

--- src/Command/GenerateAllDistancesCommand.php
+++ src/Command/GenerateAllDistancesCommand.php
@@ -22,7 +22,7 @@ class GenerateAllDistancesCommand extends Command
      */
     private $entityManager;
 
-    public function __construct(string $name = null,EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager,string $name = null)
     {
         parent::__construct($name);
         $this->entityManager = $entityManager;
